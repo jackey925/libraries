0494a5d8a4381775ff921d506b997c99df306b73
季頲 <jiting@jitings-MacBook-Pro.local>
2024-10-29 08:25:41 +0800

    新增函数

--- find.go
+++ find.go
@@ -3,3 +3,12 @@ package libraries
 func T(t string) {
 	println(t)
 }
+
+func Find(slice []string, val string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
